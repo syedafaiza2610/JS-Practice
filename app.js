1ea74f9555d7c908fd120c2f1a5308c974096820
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-03 20:51:50 -0700

    practice

---
+++ app.js
@@ -0,0 +1,120 @@
+// var num1 = 300;
+// var num2 = 400;
+// // alert(num1 + num2)
+// // console.log(num1 + num2)
+// var result =(num1 + num2 *100 / 500)
+// console.log(result)
+// var firstName = "Faiza";
+// var lastName = "Naqi Uddin";
+// var result = firstName  + " " + lastName;
+// console.log(result);
+//  document.write("<h1>" +  "It is my first Js Class" + "</h1>");
+//  var myName = "Hello World";
+//  document.write(myName);
+// var _abc = "Syeda Faiza Naqi Uddin"; //_ and $ is a legal special character
+// document.write(_abc)
+
+
+// var num1 = "y64";
+// var num2 = 6;
+// var result1 = num1 + num2;
+// var result2 = num1 - num2;
+// var result3 = num1 * num2;
+// var result4 = num1 / num2;
+// var result5 = num1 % num2;
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+// console.log(result5);
+//  var firstNum = +prompt("Enter your first Number");
+//  var secondNum = +prompt("Enter your second Number");
+//  var add = firstNum + secondNum;
+//  var sub = firstNum - secondNum;
+//  var mul = firstNum * secondNum;
+//  var div = firstNum / secondNum;
+//  var mod = firstNum % secondNum;
+//   document.write(
+//     "<table border = 1>"+
+//     "<tr>"+
+//     "<th>"+ "Addition"+"</th>"+
+//     "<tr>"+"<td>"+ add+"</td>"
+//     +"</tr>"+"</tr>"+
+//     "<tr>"+
+//     "<th>"+ "Subtraction"+"</th>"+
+//     "<tr>"+"<td>"+ sub+"</td>"
+//     +"</tr>"+"</tr>"+
+//     "<tr>"+
+//     "<th>"+ "Multiplication"+"</th>"+
+//     "<tr>"+"<td>"+ mul+"</td>"
+//     +"</tr>"+"</tr>"+
+//     "<tr>"+
+//     "<th>"+ "Division"+"</th>"+
+//     "<tr>"+"<td>"+ div+"</td>"
+//     +"</tr>"+"</tr>"+
+//     "<tr>"+
+//     "<th>"+ "Modulo"+"</th>"+
+//     "<tr>"+"<td>"+ mod+"</td>"
+//     +"</tr>"+"</tr>"
+
+
+//     + "</table>"
+//   )
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
